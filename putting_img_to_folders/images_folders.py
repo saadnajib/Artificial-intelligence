ddc42d98cf8d0614f3bf20378688b86e57d25a0b
Muhammad Saad Najib <45318508+saadnajib@users.noreply.github.com>
2021-03-01 13:07:38 -0800

    Update images_folders.py

--- putting_img_to_folders/images_folders.py
+++ putting_img_to_folders/images_folders.py
@@ -1,4 +1,4 @@
-##################################best logic
+
 import glob
 import shutil
 import pandas as pd
@@ -19,16 +19,7 @@ for i in name_list:
         print("dir")
 #         count=count+1
 
-# for i in name_list:
-#     if os.path.exists(i):
-#         for j in name_list_image:
-#             metadata=j.split('\\')[-1].replace(' ', '').replace('right', '').replace('random', '').replace('left', '').replace('front', '').replace('.png', '')
-#             if i==metadata:
-#                 ppath=i+"/"+j
-#                 if not os.path.exists(ppath):
-#                     cv2.imwrite(i+"/"+j,cv2.imread(j))
-#                     print("image_put")
-                
+
 for i in name_list:
     if os.path.exists(i):
         for j in name_list_image:
@@ -37,20 +28,4 @@ for i in name_list:
                 cv2.imwrite(i+"/"+j,cv2.imread(j))
                 print("image_put")
 
-        
-# C:\Users\saadn\Desktop\subfolder
-# metadata = []
-
-# # read image as grey scale
-# grey_img = cv2.imread("akhtar_khan_20005608_left.png")
-# metadata.append(grey_img)
-# # save image
-# status = cv2.imwrite('pic/saad.png',metadata[0])
-
-# print("Image written to file-system : ",status)
-        
-        
-        
-        
-# print(name_list[2])
-# print(name_list_image[2])
+   
