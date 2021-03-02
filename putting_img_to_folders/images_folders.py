2af4f5d38260ad8376112333d1e6f6873837db81
Muhammad Saad Najib <45318508+saadnajib@users.noreply.github.com>
2021-03-02 04:02:30 -0800

    updated
    all you need to do is put this file in a folder where you want to create images folders and put all your images which you want to assign in a seperate folder and give that folder's path in the script. 
    (You might need to change .replace() as per your preference)
    all done now run it.

--- putting_img_to_folders/images_folders.py
+++ putting_img_to_folders/images_folders.py
@@ -5,8 +5,8 @@ import pandas as pd
 import cv2
 import os
 
-
-photos = glob.glob(r'*')
+#enter your images path here 
+photos = glob.glob(r'D:/saad usb/face_recognination_faceNet_triplet_loss/Tess_facenet_code/research-papers/facenet/new/*')
 name_list = []
 name_list_image = []
 for p in photos:
@@ -19,13 +19,13 @@ for i in name_list:
         print("dir")
 #         count=count+1
 
-
 for i in name_list:
     if os.path.exists(i):
+        
         for j in name_list_image:
             metadata=j.split('\\')[-1].replace(' ', '').replace('right', '').replace('random', '').replace('left', '').replace('front', '').replace('.png', '')
+            print(metadata)
             if i==metadata:              
-                cv2.imwrite(i+"/"+j,cv2.imread(j))
+                cv2.imwrite(""+i+"/"+j,cv2.imread("D:/saad usb/face_recognination_faceNet_triplet_loss/Tess_facenet_code/research-papers/facenet/new/"+j))
                 print("image_put")
 
-   
